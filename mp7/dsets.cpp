bdaa8b5e3928e0e5ae25f862e2798b70a7cf5dba
yuliangfan <yf3@illinois.edu>
2018-04-23 23:46:14 -0500

    part1

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,33 @@
-/* Your code here! */
+#include "dsets.h"
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int DisjointSets::find(int x){
+  if (arr[x]<0)
+    return x;
+  else
+    return arr[x]=find(arr[x]);
+}
+
+void DisjointSets::setunion(int a,int b){
+//a,b has to be roots.
+  int r1=find(a);
+  int r2=find(b);
+  int newsize=arr[r1]+arr[r2];
+  if (r1==r2) return;
+  if (arr[r1]<=arr[r2]){//find the root with bigger absolute value, points to it.
+    arr[r2]=r1;
+    arr[r1]=newsize;
+  }
+  else{
+    arr[r1]=r2;
+    arr[r2]=newsize;
+  }
+}
+
+void DisjointSets::addelements(int num){
+  for (int i=0;i<num;i++){
+    arr.push_back(-1);
+  }
+}
--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,14 @@
-/* Your code here! */
+#ifndef DSET_H
+#define  DSET_H
+#include <algorithm>
+#include <vector>
+using namespace std;
+class DisjointSets{
+public:
+  void setunion(int r1,int r2);
+  void addelements(int num);
+  int find(int x);
+private:
+  vector<int> arr;
+ };
+#endif
